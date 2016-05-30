319985068f19dc601d53bb36710d5138a7c81af3
Christo <Christo>
2016-05-30 12:16:11 +0100

    added Hello World example

---
+++ grails-app/controllers/grailstest/HelloController.groovy
@@ -0,0 +1,9 @@
+package grailstest
+
+// http://docs.grails.org/latest/guide/gettingStarted.html#aHelloWorldExample
+class HelloController {
+
+    def index() {
+        render "Hello World!"
+    }
+}
